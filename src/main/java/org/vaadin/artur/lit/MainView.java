3e07c5450251ad534ccac5401ff4b898a4a61728
Artur Signell <artur@vaadin.com>
2019-05-13 18:01:56 +0300

    Fix extra horizontal scrollbar

--- src/main/java/org/vaadin/artur/lit/MainView.java
+++ src/main/java/org/vaadin/artur/lit/MainView.java
@@ -3,9 +3,11 @@ package org.vaadin.artur.lit;
 import org.vaadin.artur.github_corner.GitHubCorner;
 
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.page.BodySize;
 import com.vaadin.flow.router.Route;
 
 @Route("")
+@BodySize(width = "100%")
 public class MainView extends Div {
 
     public MainView() {
